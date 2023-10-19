9495d5cd7628b2fba683ccbde37a0e44443301d8
Veronica Clavijo <veuge.clavijo@gmail.com>
2023-10-19 22:11:02 +0200

    CORS

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,11 @@ import invoiceRoutes from './routes/invoiceRoute';
 
 const app = express();
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 const PORT = 3000;
 
